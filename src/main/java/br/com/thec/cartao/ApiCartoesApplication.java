1fa9a7c4e38db6479e121d9e65d0c7bc91bd7665
THIAGO CORREA <thiagofarbo@gmail.com>
2020-12-08 23:20:35 -0300

    Criado configuracao para analise com Prometheus e Grafana

--- src/main/java/br/com/thec/cartao/ApiCartoesApplication.java
+++ src/main/java/br/com/thec/cartao/ApiCartoesApplication.java
@@ -1,10 +1,15 @@
 package br.com.thec.cartao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 
 @EnableAsync
@@ -15,5 +20,11 @@ public class ApiCartoesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCartoesApplication.class, args);
+	}
+	
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+		System.out.println("******************"+applicationName+"******************");
+	    return registry -> registry.config().commonTags("application", applicationName);
 	}
 }
